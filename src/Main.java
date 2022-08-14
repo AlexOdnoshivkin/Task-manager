ed74f5a8942571219422b5345575392b2d089559
Alex Odnoshivkin <alexander.odnoshivkin@yandex.ru>
2022-08-14 17:41:21 +0300

    fix: update README.md

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import http.KVServer;
+import manager.Managers;
+import manager.TaskManager;
+
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-
-
+        try {
+            KVServer server = new KVServer();
+            server.start();
+            TaskManager manager = Managers.getDefault();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
